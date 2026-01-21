17e6e92691fd2a063f1a670e29ceae8eaeabd7ba
hata-okrc <dahuiqiaoben549@gmail.com>
2026-01-21 09:22:14 +0900

    add shiki

--- lib/markdown.ts
+++ lib/markdown.ts
@@ -1,7 +1,22 @@
-import { remark } from 'remark';
-import html from 'remark-html';
+import { remark } from "remark";
+import remarkRehype from "remark-rehype";
+import rehypeStringify from "rehype-stringify";
+import rehypePrettyCode from "rehype-pretty-code";
+
+// export async function markdownToHtml(markdown: string): Promise<string> {
+//   const result = await remark().use(html, {sanitize: false}).process(markdown);
+//   return result.toString();
+// }
 
 export async function markdownToHtml(markdown: string): Promise<string> {
-  const result = await remark().use(html, {sanitize: false}).process(markdown);
+  const result = await remark()
+    .use(remarkRehype)
+    .use(rehypePrettyCode, {
+      theme: "one-dark-pro",
+      defaultLang: "plaintext",
+    })
+    .use(rehypeStringify)
+    .process(markdown);
+
   return result.toString();
 }
